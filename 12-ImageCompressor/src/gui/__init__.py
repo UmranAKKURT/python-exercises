bc9d2f34a426e8c900c14f0a904c6cfd39197e27
Ümran Akkurt <akkurtumrannn@gmail.com>
2026-08-23 18:53:19 +0300

    added new changes

---
+++ 12-ImageCompressor/src/gui/__init__.py
@@ -0,0 +1,13 @@
+"""
+ImageCompressor GUI package.
+
+This package contains the graphical user interface
+components of the ImageCompressor application.
+"""
+
+from .app import ImageCompressorApp
+
+
+__all__ = [
+    "ImageCompressorApp"
+]
